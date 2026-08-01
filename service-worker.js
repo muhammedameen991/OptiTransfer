dfa94ea7785fe3577a091a10b2911268d757efe9
Ameen E <ayishaameen491@gmail.com>
2026-08-01 10:53:23 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,104 @@
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
+// ============================================
+// OptiTransfer Service Worker
+// Version: 1.0.0
+// ============================================
 
-  './css/style.css',
+const CACHE_NAME = "optitransfer-v1";
 
-  './js/app.js',
-  './js/camera.js',
-  './js/decoder.js',
-  './js/fileManager.js',
-  './js/frameGenerator.js',
-  './js/utils.js'
+const STATIC_ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+
+    "./css/style.css",
+
+    "./js/app.js",
+    "./js/camera.js",
+    "./js/decoder.js",
+    "./js/fileManager.js",
+    "./js/frameGenerator.js",
+    "./js/utils.js",
+
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
+
+// Install
+self.addEventListener("install", (event) => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(STATIC_ASSETS))
+            .then(() => self.skipWaiting())
+
+    );
+
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+
+                })
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
+});
+
+// Fetch
+
+self.addEventListener("fetch", (event) => {
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+
+        caches.match(event.request).then(cacheResponse => {
+
+            if (cacheResponse) {
+
+                return cacheResponse;
+
+            }
+
+            return fetch(event.request)
+                .then(networkResponse => {
+
+                    const clone = networkResponse.clone();
+
+                    caches.open(CACHE_NAME).then(cache => {
+
+                        cache.put(event.request, clone);
+
+                    });
+
+                    return networkResponse;
+
+                })
+                .catch(() => {
+
+                    return caches.match("./index.html");
+
+                });
+
+        })
+
+    );
+
+});
